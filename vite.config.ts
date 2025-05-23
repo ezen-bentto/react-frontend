e0e6e4b874ff1714105e31482c10e481170d8925
kimsudang <0ksuyeon0@gmail.com>
2025-05-23 09:51:17 +0900

    fix: husky 에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import checker from 'vite-plugin-checker';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), checker({
+  plugins: [
+    react(),
+    tailwindcss(),
+    checker({
       typescript: true,
       eslint: {
         lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
       },
-    }) ],
-})
+    }),
+  ],
+});
