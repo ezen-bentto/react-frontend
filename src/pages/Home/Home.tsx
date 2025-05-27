e0e2a18f4d71d7d9935ed93ceab0aa09ccd6a4d0
lugia574 <66871221+lugia574@users.noreply.github.com>
2025-05-28 00:28:37 +0900

    feat : 컴포넌트 theme 세팅

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,12 @@
+import Button from "@/components/ui/Button";
+
 const Home = () => {
-  return <h1>홈 페이지</h1>;
+  return (
+    <div className="flex  justify-center items-center flex-col">
+      <h1>홈 페이지</h1>
+      <Button className="bg-my-primary text-white w-2xs">안녕</Button>
+    </div>
+  );
 };
 
 export default Home;
