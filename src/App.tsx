04c98a12d2453e24b190f4ec3763735959f34dd6
Kim Hyemi <qnlqlaqkq120@gmail.com>
2025-05-28 14:37:23 +0900

    fix: 백엔드 url 수정

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,16 @@ function App() {
 
 export default App;
 
-// 백엔드 API URL (로컬에서 백엔드가 3000번 포트에서 실행되는 경우)
-const apiUrl = "http://localhost:4000/api/demo";  // 따옴표 수정
+// 백엔드 API URL (로컬에서 백엔드가 4000번 포트에서 실행되는 경우)
+const apiUrl = "http://3.37.0.90:4000/api/demo"; // 따옴표 수정
 
-axios.get(apiUrl)
+axios
+  .get(apiUrl)
   .then(response => {
     // eslint-disable-next-line no-console
-    alert(JSON.stringify(response.data));  // 백엔드의 응답 데이터
+    alert(JSON.stringify(response.data)); // 백엔드의 응답 데이터
   })
   .catch(error => {
     // eslint-disable-next-line no-console
-    alert("API 호출 실패:" + error);  // 따옴표 수정
+    alert("API 호출 실패:" + error); // 따옴표 수정
   });
