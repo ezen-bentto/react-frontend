523aec20be884b403597df31f5d0522077a1e356
Kim Hyemi <qnlqlaqkq120@gmail.com>
2025-05-28 14:29:34 +0900

    test: 프론트-백 api 연결 test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import "./App.css";
 import AppRouter from "./routers/AppRouter";
 
@@ -6,3 +7,16 @@ function App() {
 }
 
 export default App;
+
+// 백엔드 API URL (로컬에서 백엔드가 3000번 포트에서 실행되는 경우)
+const apiUrl = "http://localhost:4000/api/demo";  // 따옴표 수정
+
+axios.get(apiUrl)
+  .then(response => {
+    // eslint-disable-next-line no-console
+    alert(JSON.stringify(response.data));  // 백엔드의 응답 데이터
+  })
+  .catch(error => {
+    // eslint-disable-next-line no-console
+    alert("API 호출 실패:" + error);  // 따옴표 수정
+  });
