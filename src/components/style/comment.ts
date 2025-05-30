737301f002674b4ac6127c31ecf00f39d0ca5fde
lugia574 <66871221+lugia574@users.noreply.github.com>
2025-05-30 20:33:53 +0900

    style : comment 컴포넌트 css 수정정

--- src/components/style/comment.ts
+++ src/components/style/comment.ts
@@ -1,13 +1,13 @@
 import { cva, type VariantProps } from "class-variance-authority";
 
-export const comment = cva("item-center flex gap-4 p-4 size-full", {
+export const comment = cva("item-center flex gap-4 p-4 size-full rounded-lg shadow-sm", {
   variants: {
     size: {
       md: "max-w-sm text-base",
       lg: "max-w-full text-xl",
     },
     intent: {
-      default: "border-brand-primary bg-white shadow-md",
+      default: "border-brand-primary bg-white",
     },
   },
   defaultVariants: {
