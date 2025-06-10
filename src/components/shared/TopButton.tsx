a06454c3df1091db6a2c76ba9aaf2296ac89c578
bbwest0709 <bbwest0709@gmail.com>
2025-06-11 08:58:03 +0900

    chore : top 버튼 주석 추가

--- src/components/shared/TopButton.tsx
+++ src/components/shared/TopButton.tsx
@@ -2,6 +2,24 @@ import { useEffect, useState } from "react";
 import { topBtn } from "../style/topBtn";
 import { UpOutlined } from "@ant-design/icons";
 
+/**
+ *
+ * TopButton 컴포넌트
+ * 화면 우측 하단에 위치하며, 스크롤 위치가 일정 이상일 때 나타나고 클릭 시 최상단으로 이동
+ * `cva`를 통해 보여짐 여부만 제어하며, 배경 이미지와 스타일은 Tailwind로 구성
+ *
+ * @function TopButton.tsx
+ * @date 2025/06/11
+ * @history
+ * -------------------------------------------------------
+ *           변경일             작성자             변경내용
+ * -------------------------------------------------------
+ *
+ *        2025/06/11           이철욱               최초 작성 - cva 기반 마이그레이션
+ *
+ * @returns 화면 우측 하단 고정 Top 버튼
+ */
+
 const TopButton = () => {
   const [visible, setVisible] = useState(false);
 
