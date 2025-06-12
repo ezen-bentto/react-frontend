127fa1d048cab99beed497f593d64830de2d6968
lugia574 <66871221+lugia574@users.noreply.github.com>
2025-06-12 21:25:39 +0900

    style : main 배너 수정중

--- src/components/home/TextSlider.tsx
+++ src/components/home/TextSlider.tsx
@@ -4,12 +4,16 @@ interface props {
   duration: string;
   title: string;
   summary: string;
+  textColor: string;
 }
 
 const TextSlider = ({ slides, activeIndex }: { slides: props[]; activeIndex: number }) => {
   return (
     <div className="flex-default w-full order-2 md:order-1">
-      <div key={activeIndex} className="text-white animate-fadeSlide flex flex-col">
+      <div
+        key={activeIndex}
+        className={`text-${slides[activeIndex].textColor} animate-fadeSlide flex flex-col`}
+      >
         <p className="text-xl md:text-2xl">{slides[activeIndex].duration}</p>
         <h2 className="text-4xl md:text-5xl font-bold leading-snug whitespace-pre-line break-words line-clamp-4">
           {slides[activeIndex].title}
