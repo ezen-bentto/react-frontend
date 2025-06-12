644829a01230f1101b03512f5d587541ab40ed6a
lugia574 <dlcjfdnr789@naver.com>
2025-06-12 14:57:00 +0900

    feat : constest zustand 구현 중

---
+++ src/types/contestType.ts
@@ -0,0 +1,26 @@
+export type Contest = {
+  id: number;
+  writer_id: number;
+  title: string;
+  img: string | null;
+  organizer: string;
+  organizer_type: string;
+  participants: string;
+  prize: string;
+  start_date: string;
+  end_date: string;
+  homepage: string;
+  benefits: string;
+  contest_tag: string;
+  article: string;
+  views: number;
+  reg_date: string;
+};
+
+export interface ContestStore {
+  contests: Contest[];
+  popularContests: Contest[];
+  latestContests: Contest[];
+
+  fetchContest: () => void;
+}
