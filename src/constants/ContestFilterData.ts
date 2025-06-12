7fb2cbf2ae96c5fa9d0ebf14ad59965837e273a3
YUL554 <hyr0197@gmail.com>
2025-06-13 00:13:04 +0900

    feat : 공모전 필터링 구현

---
+++ src/constants/ContestFilterData.ts
@@ -0,0 +1,38 @@
+import type { FilterGroup } from "@/components/shared/Fillter";
+
+export const contestFilterData: FilterGroup[] = [
+  {
+    name: "field",
+    label: "분야",
+    options: [
+      { label: "포스터/웹툰/콘텐츠", value: "포스터/웹툰/콘텐츠" },
+      { label: "사진/영상/UCC", value: "사진/영상/UCC" },
+      { label: "아이디어/기획", value: "기획/아이디어" },
+      { label: "IT/학술/논문", value: "IT/학술/논문" },
+      { label: "네이밍/슬로건", value: "네이밍/슬로건" },
+      { label: "스포츠/음악", value: "스포츠/음악" },
+      { label: "미술/디자인/건축", value: "디자인/순수미술/공예" },
+    ],
+    multiSelect: true,
+  },
+  {
+    name: "ageGroup",
+    label: "연령",
+    options: [
+      { label: "대학생", value: "대학생" },
+      { label: "직장인/일반인", value: "직장인/일반인" },
+      { label: "제한없음", value: "대상 제한 없음" },
+    ],
+  },
+  {
+    name: "organizerType",
+    label: "기업",
+    options: [
+      { label: "대기업", value: "대기업" },
+      { label: "중견/중소기업", value: "중소기업" },
+      { label: "공공기관/공기업", value: "공공기관/공기업" },
+      { label: "비영리단체/협회/재단", value: "영리단체/협회/재단" },
+    ],
+    multiSelect: true,
+  },
+];
--- src/pages/Contest/ContestList.tsx
+++ src/pages/Contest/ContestList.tsx
@@ -1,37 +1,89 @@
-import { featchContestlist } from "@/api/contest/list";
 import Card from "@/components/shared/Card";
-import type { Contest } from "@/types/contestType";
+import Fillter from "@/components/shared/Fillter";
+import { contestFilterData } from "@/constants/ContestFilterData";
+import { useContestStore } from "@/store/contest/useContest";
 import countDate from "@/utils/countDate";
 import { useEffect, useState } from "react";
 
 const ContestList = () => {
-  const [items, setItems] = useState<Contest[]>([]);
 
-  const fetchData = async () => {
-    try {
-      const data = await featchContestlist();
-      setItems(data);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  const { popularContests, fetchContest } = useContestStore();
+  const [category, setCategory] = useState<string[]>([]);
+  const [age, setAge] = useState<string[]>([]);
+  const [organizerType, setOrganizerType] = useState<string[]>([]);
+  const [filteredContests, setFilteredContests] = useState(popularContests);
+  // const isFiltered = category.length > 0 || age.length > 0 || organizerType.length > 0;
+
+  // const [items, setItems] = useState<Contest[]>([]);
+
+  // const fetchData = async () => {
+  //   try {
+  //     const data = await featchContestlist();
+  //     console.info(data);
+  //     setItems(data);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   useEffect(() => {
-    fetchData();
+    fetchContest();
   }, []);
+
+  // 전체 공모전 불러오기
+  useEffect(() => {
+    if (popularContests) {
+      setFilteredContests(popularContests);
+    }
+  }, [popularContests]);
+
+  useEffect(() => {
+    console.info(category, age, organizerType);
+    if (!popularContests) return;
+
+    const filtered = popularContests.filter(item => {
+      const contestTag = item.contest_tag.split(",")[0].trim();
+      const selectedCategory = category.length === 0 || category.includes(contestTag);
+      const selectedAge = age.length === 0 || age.includes(item.participants);
+      const selectedOrganizer =
+        organizerType.length === 0 || organizerType.includes(item.organizer_type);
+
+      console.info(item.contest_tag, typeof item.contest_tag);
+      console.info(category);
+      return selectedCategory && selectedAge && selectedOrganizer;
+    });
+
+    setFilteredContests(filtered);
+  }, [category, age, organizerType]);
 
   return (
     <div>
       {/* 필터 */}
-      <div></div>
+      <div className="py-5">
+        <Fillter
+          filters={contestFilterData}
+          onFilterChange={(group, value) =>
+            group === "field"
+              ? setCategory(value)
+              : group === "ageGroup"
+                ? setAge(value)
+                : setOrganizerType(value)
+          }
+          onSearchSubmit={() => console.info("검색 핸들러")}
+        />
+      </div>
       {/* 배너 */}
 
       {/* 카드 리스트 */}
       <div className="flex gap-6 flex-wrap justify-center mt-8">
-        {items.length === 0 ? (
+        {filteredContests.length === 0 ? (
           <p>데이터 로딩 중...</p>
         ) : (
-          items.map(item => (
+          filteredContests.map(item => (
             <Card
               key={item.id}
               dday={countDate(item.end_date).toString()}
