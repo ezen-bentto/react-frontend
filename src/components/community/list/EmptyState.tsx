6feba002982a13f8d993a8716dba713ec0423565
Kim Hyemi <qnlqlaqkq120@gmail.com>
2025-06-27 23:48:08 +0900

    fix: 이미지 외 불필요 문구 제거

--- src/components/community/list/EmptyState.tsx
+++ src/components/community/list/EmptyState.tsx
@@ -4,7 +4,6 @@ const EmptyState = ({ imgSrc }: EmptyStateProps) => {
     return (
         <div className="flex flex-col items-center mt-8">
             <img src={imgSrc} alt="게시글 없음" className="w-64 md:w-80 h-auto" />
-            <p className="text-gray-500 dark:text-gray-400 mt-4">게시글이 없습니다.</p>
         </div>
     );
 };
