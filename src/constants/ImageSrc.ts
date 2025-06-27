49bcb79b7221db2ef322d928635dbd3cc2916f28
lugia574 <66871221+lugia574@users.noreply.github.com>
2025-06-27 23:37:15 +0900

    feat : 대체이미지 col 추가

--- src/constants/ImageSrc.ts
+++ src/constants/ImageSrc.ts
@@ -3,3 +3,4 @@ export const DARKIMG = "/images/404_image_dark.png";
 export const LiGHT_NOT_ITEM = "/images/empty_list.png";
 export const DARK_NOT_ITEM = "/images/empty_list_dark.png";
 export const NOT_IMAGE = "/images/not_Image.png";
+export const NOT_IMAGE_COL = "/images/not_Image_col.png";
